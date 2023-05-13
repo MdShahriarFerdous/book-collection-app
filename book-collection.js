614b41eade62731fe09f682e8144472148669de9
Shahriar Ferdous <shahriarferdous2233@gmail.com>
2023-05-13 09:19:23 +0600

    used app.route() method

--- book-collection.js
+++ book-collection.js
@@ -19,13 +19,14 @@ app.get("/", function (req, res) {
 let books = [];
 
 // a get route that returns json array of books
-app.get("/books", function (req, res) {
+app.route("/books")
+
+.get(function (req, res) {
 	let bookCollection = res.json(books);
 	res.send(bookCollection);
-});
-
+})
 // a post route to add books in the empty array
-app.post("/books", function (req, res) {
+.post(function (req, res) {
 	let bookDetailsJsonData = req.body;
 	// Check if title and author properties exist
 	if (!bookDetailsJsonData.title || !bookDetailsJsonData.author) {
